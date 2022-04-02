c03aaf3216f462dfb358c79a7d2b049663e49700
abhay-lodhi <abhaylodhiab11@gmail.com>
2022-04-02 17:42:45 +0530

    fragment_add

--- app/src/main/java/com/example/finanance/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/finanance/ui/dashboard/DashboardFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.finanance.R
 import com.example.finanance.databinding.FragmentDashboardBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class DashboardFragment : Fragment() {
 
@@ -16,6 +18,25 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    override fun onResume() {
+        super.onResume()
+        val category=resources.getStringArray(R.array.category)
+        val arrayadapter=ArrayAdapter(requireContext(),R.layout.dropdown_category,category)
+        binding.autoCompleteTextView.setAdapter(arrayadapter)
+//        val autocomplete=getView()?.findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+//        autocomplete?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//
+//            }
+//
+//        }
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +45,6 @@ class DashboardFragment : Fragment() {
     ): View {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
